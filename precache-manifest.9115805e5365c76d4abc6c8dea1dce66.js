f37964518f55261c55a043914a45b0c57f39ef39
Aleksandra Pawelec <58439839+AleksandraPawelec@users.noreply.github.com>
2020-02-06 12:18:31 +0100

    Updates

---
+++ precache-manifest.9115805e5365c76d4abc6c8dea1dce66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6093799a3df9ce045cf2ff59590a683",
+    "url": "/ToDoApp/index.html"
+  },
+  {
+    "revision": "37de4d8606384812f665",
+    "url": "/ToDoApp/static/css/main.5c91db5f.chunk.css"
+  },
+  {
+    "revision": "5a3f5dc32f0249ddc788",
+    "url": "/ToDoApp/static/js/2.31a9a731.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ToDoApp/static/js/2.31a9a731.chunk.js.LICENSE"
+  },
+  {
+    "revision": "37de4d8606384812f665",
+    "url": "/ToDoApp/static/js/main.0629ab2f.chunk.js"
+  },
+  {
+    "revision": "0b1ca7cc0771e39a4195",
+    "url": "/ToDoApp/static/js/runtime-main.48ebde6e.js"
+  }
+]);
